cbb722c0a2aeb4b9208e889c1dc3b19e74f817d3
stanislavglazko <stanislavglazko@MacBook-Air-Apple.local>
2021-01-19 19:14:40 +0300

    delete names of types

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -8,6 +8,6 @@ def fetch_spacex_launch(flight_number=64, folder='images'):
     payload = {'flight_number': flight_number}
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    list_of_links = response.json()[0]['links']['flickr_images']
-    for img_number, img in enumerate(list_of_links):
+    links = response.json()[0]['links']['flickr_images']
+    for img_number, img in enumerate(links):
         load_img(f'spacex{str(img_number+1)}.jpg', img, folder)
